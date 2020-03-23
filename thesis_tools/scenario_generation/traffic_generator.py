bdcf8b307aa797c08ef154a7f02264baaeca8f61
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-03-23 21:55:08 +0100

    Added traffic generator to thesis_tools

--- thesis_tools/scenario_generation/traffic_generator.py
+++ thesis_tools/scenario_generation/traffic_generator.py
@@ -1,9 +1,12 @@
 import json
 import parameters
 import random
+import os
 
 # Create empty aircraft dictionary
 aircraft = {}
+# Create empty aircraft data list (each index corresponds to mission index)
+aircraft_data = []
 
 # Initialize ranomizer
 random.seed()
@@ -23,6 +26,7 @@ aircraft[aircraft_name]["engines"] = "[( engine: power (kW) )]"
 # Loop over number of missions to generate aircraft
 for i in range(parameters.N_missions):
     # Loop over the number of vehicles per mission:
+    aircraft_data.append([])
     for j in range(parameters.N_vehicles):
         aircraft_name = "UAV_" + str(i) + "_" + str(j) # UAV_[mission number]_[vehicle number]
         aircraft[aircraft_name] = {}
@@ -56,8 +60,23 @@ for i in range(parameters.N_missions):
         aircraft[aircraft_name]["envelop"]["h_max"] = 5000
         aircraft[aircraft_name]["envelop"]["d_range_max"] = 7
 
+        data_entry = {}
+        data_entry['id'] = aircraft_name
+        data_entry['type'] = aircraft_type
+        data_entry['v_min'] = v_min
+        data_entry['v_max'] = v_max
+        aircraft_data[i].append(data_entry)
+
 json_data = json.dumps(aircraft, indent = 4)
 
 aircraft_file = open("data/performance/OpenAP/rotor/aircraft_experiment.json", "w")   
 aircraft_file.write(json_data)
 aircraft_file.close()
+
+location = "thesis_tools/data"
+if not os.path.exists(location):
+    os.makedirs(location)
+aircraft_data_json = json.dumps(aircraft_data, indent=4)
+aircraft_json_file = open(location + "/aircraft.json", "w")
+aircraft_json_file.write(aircraft_data_json)
+aircraft_json_file.close()
