de00bbb55c39bacaf8fc057f0c39008d586ec491
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-04-04 21:01:30 +0200

    Run all generators in the right order by running generate_data.py

---
+++ thesis_tools/scenario_generation/generate_data.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("python thesis_tools/scenario_generation/traffic_generator.py")
+os.system("python thesis_tools/scenario_generation/scenario_generator.py")
+os.system("python thesis_tools/scenario_generation/wind_generator.py")
+os.system("python thesis_tools/scenario_generation/route_generator.py")
+os.system("python thesis_tools/scenario_generation/geofence_generator.py")
