fe8cc5342b5873bd3c82abda5232f08ee7b853c8
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-04-07 10:46:50 +0200

    Created function for scenario generator

--- thesis_tools/scenario_generation/generate_data.py
+++ thesis_tools/scenario_generation/generate_data.py
@@ -1,8 +1,9 @@
 import os
 import traffic_generator
+import scenario_generator
 
 traffic_generator.generate_traffic()
-os.system("python thesis_tools/scenario_generation/scenario_generator.py")
+scenario_generator.generate_scenario()
 os.system("python thesis_tools/scenario_generation/wind_generator.py")
 os.system("python thesis_tools/scenario_generation/route_generator.py")
 os.system("python thesis_tools/scenario_generation/geofence_generator.py")
--- thesis_tools/scenario_generation/scenario_generator.py
+++ thesis_tools/scenario_generation/scenario_generator.py
@@ -4,53 +4,55 @@ import random
 import json
 import os
 
-# Check if there is a aircraft.json, otherwise stop
-if (not os.path.isfile("thesis_tools/data/aircraft.json")):
-    print("first generate traffic using traffic_generator.py")
-    exit()
+def generate_scenario():
 
-# Initialize randomizer
-random.seed()
+    # Check if there is a aircraft.json, otherwise stop
+    if (not os.path.isfile("thesis_tools/data/aircraft.json")):
+        print("first generate traffic using traffic_generator.py")
+        exit()
 
-# create dictionary with scenario data
-scenario_data = []
+    # Initialize randomizer
+    random.seed()
 
-# Load aircraft data from aircraft.json
-aircraft_json = open("thesis_tools/data/aircraft.json", "r")
-aircraft_data = json.load(aircraft_json)
-aircraft_json.close()
+    # create dictionary with scenario data
+    scenario_data = []
 
-for i in range(parameters.N_missions):
-    data_entry = {}
-    # It is assumed that 2 aircraft are involved in a mission
-    spd0 = None
-    if (aircraft_data[i][0]['type'] == 'RC'):
-        spd0 = random.uniform(parameters.min_vel_RC, aircraft_data[i][0]['v_max'])
-    else:
-        spd0 = random.uniform(aircraft_data[i][0]['v_min'], aircraft_data[i][0]['v_max'])
-    data_entry['spd0'] = spd0
+    # Load aircraft data from aircraft.json
+    aircraft_json = open("thesis_tools/data/aircraft.json", "r")
+    aircraft_data = json.load(aircraft_json)
+    aircraft_json.close()
 
-    hdg0 = random.uniform(0., 360.)
-    data_entry['hdg0'] = hdg0
+    for i in range(parameters.N_missions):
+        data_entry = {}
+        # It is assumed that 2 aircraft are involved in a mission
+        spd0 = None
+        if (aircraft_data[i][0]['type'] == 'RC'):
+            spd0 = random.uniform(parameters.min_vel_RC, aircraft_data[i][0]['v_max'])
+        else:
+            spd0 = random.uniform(aircraft_data[i][0]['v_min'], aircraft_data[i][0]['v_max'])
+        data_entry['spd0'] = spd0
 
-    spd1 = None
-    if (aircraft_data[i][1]['type'] == 'RC'):
-        spd1 = random.uniform(parameters.min_vel_RC, aircraft_data[i][1]['v_max'])
-    else:
-        spd1 = random.uniform(aircraft_data[i][1]['v_min'], aircraft_data[i][1]['v_max'])
-    data_entry['spd1'] = spd1
+        hdg0 = random.uniform(0., 360.)
+        data_entry['hdg0'] = hdg0
 
-    d_psi = random.uniform(0., 360.)
-    data_entry['d_psi'] = d_psi
+        spd1 = None
+        if (aircraft_data[i][1]['type'] == 'RC'):
+            spd1 = random.uniform(parameters.min_vel_RC, aircraft_data[i][1]['v_max'])
+        else:
+            spd1 = random.uniform(aircraft_data[i][1]['v_min'], aircraft_data[i][1]['v_max'])
+        data_entry['spd1'] = spd1
 
-    dist_cpa = random.uniform(0., parameters.R_PZ)
-    data_entry['dist_cpa'] = dist_cpa
+        d_psi = random.uniform(0., 360.)
+        data_entry['d_psi'] = d_psi
 
-    scenario_data.append(data_entry)
+        dist_cpa = random.uniform(0., parameters.R_PZ)
+        data_entry['dist_cpa'] = dist_cpa
 
-# Write data to json file
-location = "thesis_tools/data"
-scenario_data_json = json.dumps(scenario_data, indent=4)
-scenario_json_file = open(location + "/scenario.json", "w")
-scenario_json_file.write(scenario_data_json)
-scenario_json_file.close()
+        scenario_data.append(data_entry)
+
+    # Write data to json file
+    location = "thesis_tools/data"
+    scenario_data_json = json.dumps(scenario_data, indent=4)
+    scenario_json_file = open(location + "/scenario.json", "w")
+    scenario_json_file.write(scenario_data_json)
+    scenario_json_file.close()
