7210fe07ca3ec67bdad012a900c271884c8e6b0f
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-04-07 10:59:35 +0200

    Created function for wind generator

--- thesis_tools/scenario_generation/generate_data.py
+++ thesis_tools/scenario_generation/generate_data.py
@@ -1,9 +1,10 @@
 import os
 import traffic_generator
 import scenario_generator
+import wind_generator
 
 traffic_generator.generate_traffic()
 scenario_generator.generate_scenario()
-os.system("python thesis_tools/scenario_generation/wind_generator.py")
+wind_generator.generate_wind()
 os.system("python thesis_tools/scenario_generation/route_generator.py")
 os.system("python thesis_tools/scenario_generation/geofence_generator.py")
--- thesis_tools/scenario_generation/wind_generator.py
+++ thesis_tools/scenario_generation/wind_generator.py
@@ -4,39 +4,42 @@ import json
 import random
 import os
 
-# Check if there is a aircraft.json, otherwise stop
-if (not os.path.isfile("thesis_tools/data/aircraft.json")):
-    print("first generate traffic using traffic_generator.py")
-    exit()
+def generate_wind():
 
-# Initialize randomizer
-random.seed()
+    # Check if there is a aircraft.json, otherwise stop
+    if (not os.path.isfile("thesis_tools/data/aircraft.json")):
+        print("first generate traffic using traffic_generator.py")
+        exit()
 
-# Create wind dictionary (index corresponds to mission number)
-wind_data = []
+    # Initialize randomizer
+    random.seed()
 
-# Load aircraft data from aircraft.json
-aircraft_json = open("thesis_tools/data/aircraft.json", "r")
-aircraft_data = json.load(aircraft_json)
-aircraft_json.close()
+    # Create wind dictionary (index corresponds to mission number)
+    wind_data = []
 
-for i in range(parameters.N_missions):
-    data_entry = {}
-    # Determine v_max
-    v_max = None
-    for j in range(len(aircraft_data[i])):
-        if (j == 0):
-            v_max = aircraft_data[i][j]['v_max']
-        else:
-            v_max = min(v_max, aircraft_data[i][j]['v_max'])
+    # Load aircraft data from aircraft.json
+    aircraft_json = open("thesis_tools/data/aircraft.json", "r")
+    aircraft_data = json.load(aircraft_json)
+    aircraft_json.close()
 
-    data_entry['speed'] = random.uniform(0, v_max) # Wind speed [m/s]
-    data_entry['direction'] = random.uniform(0., 360.) # Direction from which the wind is blowing
-    wind_data.append(data_entry)
+    for i in range(parameters.N_missions):
+        data_entry = {}
+        # Determine v_max
+        v_max = None
+        for j in range(len(aircraft_data[i])):
+            if (j == 0):
+                v_max = aircraft_data[i][j]['v_max']
+            else:
+                v_max = min(v_max, aircraft_data[i][j]['v_max'])
 
-# Write data to json file
-location = "thesis_tools/data"
-wind_data_json = json.dumps(wind_data, indent=4)
-wind_json_file = open(location + "/wind.json", "w")
-wind_json_file.write(wind_data_json)
-wind_json_file.close()
+        data_entry['speed'] = random.uniform(0, v_max) # Wind speed [m/s]
+        data_entry['direction'] = random.uniform(0., 360.) # Direction from which the wind is blowing
+        wind_data.append(data_entry)
+
+    # Write data to json file
+
+    location = "thesis_tools/data"
+    wind_data_json = json.dumps(wind_data, indent=4)
+    wind_json_file = open(location + "/wind.json", "w")
+    wind_json_file.write(wind_data_json)
+    wind_json_file.close()
