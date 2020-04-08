6f6263685d889a4abff71161e7d30dfff6776784
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-04-08 21:38:31 +0200

    Fixed and simplified geofence generator

--- thesis_tools/scenario_generation/geofence_generator.py
+++ thesis_tools/scenario_generation/geofence_generator.py
@@ -27,6 +27,25 @@ def intersect_angle_ranges(angles1, angles2):
     max_angle = min(angles1[1], angles2[1])
     return np.array([min_angle, max_angle])
 
+def get_trk_range(trk0, trk1):
+    # First assume trk0 is minimum
+    trk_0 = trk0
+    trk_1 = trk1
+    if (trk_1 < trk_0):
+        trk_1 += 2. * np.pi
+    if ((trk_1 - trk_0) < np.pi):
+        angle_min = trk_0
+        angle_max = trk_1
+        return angle_min, angle_max
+    
+    trk_0 = trk0
+    trk_1 = trk1
+    if (trk_0 < trk_1):
+        trk_0 += 2. * np.pi
+    angle_min = trk_1
+    angle_max = trk_0
+    return angle_min, angle_max
+
 def generate_geofence(loc_route, loc_output):
 
     # Check if there is a route.json, otherwise stop
@@ -56,121 +75,30 @@ def generate_geofence(loc_route, loc_output):
             data_entry['qdr'] = []
             data_entry['dist'] = []
 
-            # Read route points
-            points = route_data[i][j]['points']
+            # Determine outer points of routes CCW
+            route_hull = ConvexHull(points = np.array(route_data[i][j]['points']))
+            points = np.array(route_data[i][j]['points'])[route_hull.vertices].tolist()
 
-            # Create route segments
-            route_segments = []
-            for k in range(len(points) - 1):
-                route_segment = RouteSegment(points[k], points[k + 1])
-                route_segments.append(route_segment)
-
-            # Loop over points and evaluate possible ranges of geofence points
             for k in range(len(points)):
-                # First point
-                if (k == 0): 
-                    points_right = False
-                    points_left = False
-                    for m in np.arange(2, len(points)):
-                        # Check if points at right
-                        if (np.dot(points[m], route_segments[0].n_right) >= 0.):
-                            points_right = True
-                        # Check if points at left
-                        if (np.dot(points[m], route_segments[0].n_left) >= 0.):
-                            points_left = True
-                        
-                    # Set angle limits
-                    if (points_right and not points_left):
-                        angle_min = route_segments[0].trk + np.pi
-                        angle_max = route_segments[0].trk + 1.5 * np.pi
-                    elif (points_right and points_left):
-                        angle_min = route_segments[0].trk + 0.5 * np.pi
-                        angle_max = route_segments[0].trk + 1.5 * np.pi
-                    else: # if only points left
-                        angle_min = route_segments[0].trk + 0.5 * np.pi
-                        angle_max = route_segments[0].trk + np.pi 
+                segments = [None, None] # Route segments of adjacent points
 
-                # Last point
-                elif (k == (len(points) - 1 )):
-                    points_right = False
-                    points_left = False
-                    for m in range(len(points) - 2):
-                        rel_point = np.array(points[m]) - np.array(points[k])
-                        # Check if points at right
-                        if (np.dot(rel_point, route_segments[-1].n_right) <= 0.):
-                            points_right = True
-                        # Check if points at left 
-                        if (np.dot(rel_point, route_segments[-1].n_left) <= 0.):
-                            points_left= True
-                    
-                    # Set angle limits
-                    if (points_right and not points_left):
-                        angle_min = route_segments[-1].trk - 0.5 * np.pi
-                        angle_max = route_segments[-1].trk
-                    elif (points_right and points_left):
-                        angle_min = route_segments[-1].trk - 0.5 * np.pi
-                        angle_max = route_segments[-1].trk + 0.5 * np.pi
-                    else: # if only points left
-                        angle_min = route_segments[-1].trk 
-                        angle_max = route_segments[-1].trk + 0.5 * np.pi 
+                # Look at adjaccent points
 
-                # other points
+                # if first point
+                if (k == 0):
+                    segments[0] = RouteSegment(points[-1], points[k])
+                    segments[1] = RouteSegment(points[1],  points[k])
+                # if last point
+                if (k == len(points) - 1):
+                    segments[0] = RouteSegment(points[-2], points[k])
+                    segments[1] = RouteSegment(points[0],  points[k])
+                # else
                 else:
-                    # Evaluate from leg
-                    points_right = False
-                    points_left = False
-                    for m in range(len(points)):
-                        if ((m == k) or (m == (k - 1))):
-                            pass
-                        rel_point = np.array(points[m]) - np.array(points[k])
-                        # Check if points at right
-                        if (np.dot(rel_point, route_segments[k - 1].n_right) <= 0.):
-                            points_right = True
-                        # Check if points at left
-                        if (np.dot(rel_point, route_segments[k - 1].n_left) <= 0.):
-                            points_left = True
-                    
-                    # Set angle limits
-                    if (points_right and not points_left):
-                        angle_min_from = route_segments[k - 1].trk - 0.5 * np.pi
-                        angle_max_from = route_segments[k - 1].trk
-                    elif (points_right and points_left):
-                        angle_min_from = route_segments[k - 1].trk - 0.5 * np.pi
-                        angle_max_from = route_segments[k - 1].trk + 0.5 * np.pi
-                    else: # if only points left
-                        angle_min_from = route_segments[k - 1].trk 
-                        angle_max_from = route_segments[k - 1].trk + 0.5 * np.pi 
-                    angles_from = np.array([angle_min_from, angle_max_from])
+                    segments[0] = RouteSegment(points[k - 1], points[k])
+                    segments[1] = RouteSegment(points[k + 1], points[k])
 
-                    # Evaluate to leg
-                    points_right = False
-                    points_left = False
-                    for m in range(len(points)):
-                        if ((m == k) or (m == (k + 1))):
-                            pass
-                        rel_point = np.array(points[m]) - np.array(points[k])
-                        # Check if points at right
-                        if (np.dot(rel_point, route_segments[k].n_right) >= 0.):
-                            points_right = True
-                        # Check if points at left
-                        if (np.dot(rel_point, route_segments[k].n_left) >= 0.):
-                            points_left = True
-                    
-                    # Set angle limits
-                    if (points_right and not points_left):
-                        angle_min_to = route_segments[k].trk + np.pi
-                        angle_max_to = route_segments[k].trk + 1.5 * np.pi
-                    elif (points_right and points_left):
-                        angle_min_to = route_segments[k].trk + 0.5 * np.pi
-                        angle_max_to = route_segments[k].trk + 1.5 * np.pi
-                    else: # if only points left
-                        angle_min_to = route_segments[k].trk + 0.5 * np.pi
-                        angle_max_to = route_segments[k].trk + np.pi 
-                    angles_to = np.array([angle_min_to, angle_max_to])
-
-                    angle_limits = intersect_angle_ranges(angles_from, angles_to)
-                    angle_min = angle_limits[0]
-                    angle_max = angle_limits[1]
+                # Calc angle range
+                angle_min, angle_max = get_trk_range(segments[0].trk, segments[1].trk)
                 
                 angle = random.uniform(angle_min, angle_max)
                 radius = random.uniform(0., parameters.max_gf_dist)
@@ -180,10 +108,6 @@ def generate_geofence(loc_route, loc_output):
                 data_entry['points'].append(point)
                 data_entry['qdr'].append(np.rad2deg(np.arctan2(point[0], point[1])))
                 data_entry['dist'].append(np.sqrt(point[0]**2 + point[1]**2))
-            hull = ConvexHull(points = np.array(data_entry['points']))
-            data_entry['points'] = np.array(data_entry['points'])[hull.vertices].tolist()
-            data_entry['qdr'] = np.array(data_entry['qdr'])[hull.vertices].tolist()
-            data_entry['dist'] = np.array(data_entry['dist'])[hull.vertices].tolist()
             geofence_data[i].append(data_entry)
 
     # Write data to json file
