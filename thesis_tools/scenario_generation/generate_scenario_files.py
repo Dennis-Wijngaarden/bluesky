38f8cf87e17176afec59a396922a38c03a4acf89
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-04-09 10:46:28 +0200

    Updated scenario file generator to generate specific scenarios for each test series

--- thesis_tools/scenario_generation/generate_scenario_files.py
+++ thesis_tools/scenario_generation/generate_scenario_files.py
@@ -19,13 +19,23 @@ if (not os.path.isfile("thesis_tools/data/route.json")):
     print("first generate routes using route_generator.py")
     exit()
 
+# Check if there is a route_wind.json, otherwise stop
+if (not os.path.isfile("thesis_tools/data/route_wind.json")):
+    print("first generate routes using route_generator.py")
+    exit()
+
 # Check if there is a wind.json, otherwise stop
 if (not os.path.isfile("thesis_tools/data/wind.json")):
     print("first generate wind using wind_generator.py")
     exit()
 
-# Check if there is a wind.json, otherwise stop
+# Check if there is a geofence.json, otherwise stop
 if (not os.path.isfile("thesis_tools/data/geofence.json")):
+    print("first generate geofences using geofence_generator.py")
+    exit()
+
+# Check if there is a geofence_wind.json, otherwise stop
+if (not os.path.isfile("thesis_tools/data/geofence_wind.json")):
     print("first generate geofences using geofence_generator.py")
     exit()
 
@@ -41,6 +51,11 @@ route_json = open("thesis_tools/data/route.json", "r")
 route_data = json.load(route_json)
 route_json.close()
 
+# Load route.json
+route_wind_json = open("thesis_tools/data/route_wind.json", "r")
+route_wind_data = json.load(route_wind_json)
+route_wind_json.close()
+
 # Load wind.json
 wind_json = open("thesis_tools/data/wind.json", "r")
 wind_data = json.load(wind_json)
@@ -50,6 +65,11 @@ wind_json.close()
 geofence_json = open("thesis_tools/data/geofence.json", "r")
 geofence_data = json.load(geofence_json)
 geofence_json.close()
+
+# Load geofence.json
+geofence_wind_json = open("thesis_tools/data/geofence_wind.json", "r")
+geofence_wind_data = json.load(geofence_wind_json)
+geofence_wind_json.close()
 
 # Load coordinates of reference ICAO location
 icao_data = np.genfromtxt("data/navdata/airports.dat", delimiter=',', dtype=(str))
@@ -114,31 +134,71 @@ for i in range(parameters.N_missions):
 
     # Write scenario files
     # Create first UAV in scenario for all test series
-    cre_line = "00:00:00.00>CRE UAV0 UAV_" + str(i) + "_0 " + parameters.ref_position + " " + str(scenario_data[i]['hdg0']) + " " +\
+    cre_line = "00:00:00.00>CRE UAV0 UAV_" + str(i) + "_0 " + parameters.ref_position + " " + str(scenario_data[i]['trk0']) + " " +\
             str(parameters.ref_alt) + " " + str(scenario_data[i]['spd0'] / kts) + "\n"
     
-    # Create conflicitng UAV in scenario for all test series
-    conf_line = "00:00:00.00>CRECONFS UAV1 UAV_" + str(i) + "_1 UAV0 " + str(scenario_data[i]['d_psi']) + " " + str(scenario_data[i]['dist_cpa'] / nm) + " " +\
+    # Create conflicitng UAV in scenario for wind calm scenarios
+    conf_line_wind_calm = "00:00:00.00>CRECONFS UAV1 UAV_" + str(i) + "_1 UAV0 " + str(scenario_data[i]['d_psi']) + " " + str(scenario_data[i]['dist_cpa'] / nm) + " " +\
             str(parameters.t_la + parameters.t_extra) + " 0 0 " + str(scenario_data[i]['spd1'] / kts) + "\n"
 
-    # Create routes
-    wpt_lines = ""
-    for j in range(len(route_data[i][0]['points'])):
+    # Create conflicitng UAV in scenario for windy scenarios
+    conf_line_wind = "00:00:00.00>CRECONFS UAV1 UAV_" + str(i) + "_1 UAV0 " + str(scenario_data[i]['d_psi']) + " " + str(scenario_data[i]['dist_cpa'] / nm) + " " +\
+            str(parameters.t_la + parameters.t_extra) + " 0 0 " + str(route_wind_data[i][1]['gs'][0] / kts) + "\n"
+
+    # Create routes for wind calm scenarios
+    wpt_lines_wind_calm = ""
+    for j in range(1, len(route_data[i][0]['points'])):
         wp_lat, wp_lon = geo.qdrpos(lat_ref, lon_ref, route_data[i][0]['qdr'][j], route_data[i][0]['dist'][j] / nm)
-        wpt_lines += "00:00:00.00>ADDWPT UAV0 " + str(wp_lat) + "," + str(wp_lon) + "\n"
+        wpt_lines_wind_calm += "00:00:00.00>ADDWPT UAV0 " + str(wp_lat) + "," + str(wp_lon) + "\n"
 
-    for j in range(len(route_data[i][1]['points'])):
+    for j in range(1, len(route_data[i][1]['points'])):
         wp_lat, wp_lon = geo.qdrpos(lat_ref, lon_ref, route_data[i][1]['qdr'][j], route_data[i][1]['dist'][j] / nm)
-        wpt_lines += "00:00:00.00>ADDWPT UAV1 " + str(wp_lat) + "," + str(wp_lon) + "\n"
+        wpt_lines_wind_calm += "00:00:00.00>ADDWPT UAV1 " + str(wp_lat) + "," + str(wp_lon) + "\n"
 
-    # lines valid for scenario files of all test series:
-    common_lines = cre_line + conf_line + wpt_lines 
+    # Create routes for windy scenarios
+    wpt_lines_wind = ""
+    for j in range(1, len(route_wind_data[i][0]['points'])):
+        wp_lat, wp_lon = geo.qdrpos(lat_ref, lon_ref, route_wind_data[i][0]['qdr'][j], route_wind_data[i][0]['dist'][j] / nm)
+        wpt_lines_wind += "00:00:00.00>ADDWPT UAV0 " + str(wp_lat) + "," + str(wp_lon) + "\n"
 
-    # Write all common lines
-    scn_TS1.write(common_lines)
-    scn_TS2.write(common_lines)
-    scn_TS3.write(common_lines)
-    scn_TS4.write(common_lines)
+    for j in range(1, len(route_data[i][1]['points'])):
+        wp_lat, wp_lon = geo.qdrpos(lat_ref, lon_ref, route_wind_data[i][1]['qdr'][j], route_wind_data[i][1]['dist'][j] / nm)
+        wpt_lines_wind += "00:00:00.00>ADDWPT UAV1 " + str(wp_lat) + "," + str(wp_lon) + "\n"
+
+    # Create geofences for wind calm scenarios
+    gf_lines_wind_calm = "00:00:00.00>POLY GF0 "
+    for j in range(len(geofence_data[i][0]['points'])):
+        gf_lat, gf_lon = geo.qdrpos(lat_ref, lon_ref, geofence_data[i][0]['qdr'][j], geofence_data[i][0]['dist'][j] / nm)
+        gf_lines_wind_calm += str(gf_lat) + " " + str(gf_lon) + " "
+
+    gf_lines_wind_calm += "\n00:00:00.00>POLY GF1 "
+    for j in range(len(geofence_data[i][1]['points'])):
+        gf_lat, gf_lon = geo.qdrpos(lat_ref, lon_ref, geofence_data[i][1]['qdr'][j], geofence_data[i][1]['dist'][j] / nm)
+        gf_lines_wind_calm += str(gf_lat) + " " + str(gf_lon) + " "
+    
+    gf_lines_wind_calm += "\n"
+
+    # Create geofences for windy scenarios
+    gf_lines_wind = "00:00:00.00>POLY GF0 "
+    for j in range(len(geofence_data[i][0]['points'])):
+        gf_lat, gf_lon = geo.qdrpos(lat_ref, lon_ref, geofence_wind_data[i][0]['qdr'][j], geofence_wind_data[i][0]['dist'][j] / nm)
+        gf_lines_wind += str(gf_lat) + " " + str(gf_lon) + " "
+
+    gf_lines_wind += "\n00:00:00.00>POLY GF1 "
+    for j in range(len(geofence_data[i][1]['points'])):
+        gf_lat, gf_lon = geo.qdrpos(lat_ref, lon_ref, geofence_wind_data[i][1]['qdr'][j], geofence_wind_data[i][1]['dist'][j] / nm)
+        gf_lines_wind += str(gf_lat) + " " + str(gf_lon) + " "
+    
+    gf_lines_wind += "\n"
+
+    # Create wind
+    wind_line = "00:00:00.00>WIND 0 0 1000 " + str(wind_data[i]['direction']) + " " + str(wind_data[i]['speed'] / kts) + "\n"
+
+    # Write scenario files
+    scn_TS1.write(cre_line + conf_line_wind_calm + wpt_lines_wind_calm)
+    scn_TS2.write(cre_line + conf_line_wind_calm + wpt_lines_wind_calm + gf_lines_wind_calm)
+    scn_TS3.write(wind_line + cre_line + conf_line_wind + wpt_lines_wind)
+    scn_TS4.write(wind_line + cre_line + conf_line_wind + wpt_lines_wind + gf_lines_wind)
 
     for j in range(parameters.N_RS):
         batch_TS1.write("00:00:00.00>SCEN test_" + str(i) + "_TS1_RS" + str(j + 1) + "\n")
