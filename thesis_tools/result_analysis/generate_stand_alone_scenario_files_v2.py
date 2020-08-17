bd7732f595a78818072769c9b20ef06bb03d78e5
Dennis-Wijngaarden <wijngaarden.dennis@gmail.com>
2020-08-17 20:21:38 +0200

    Final bugfixes in version 2 of scenario generator

--- thesis_tools/result_analysis/generate_stand_alone_scenario_files_v2.py
+++ thesis_tools/result_analysis/generate_stand_alone_scenario_files_v2.py
@@ -66,7 +66,7 @@ if not airport_found:
     exit()
 
 # Scenario file locations and create
-loc_thesis_scns = "scenario/Thesis"
+loc_thesis_scns = "scenario/Thesis/analysis"
 loc_TS0 = loc_thesis_scns + "/TS0"
 if not os.path.exists(loc_TS0):
     os.makedirs(loc_TS0)
@@ -106,32 +106,15 @@ for f in filelist:
 filelist = [ f for f in os.listdir(loc_TS4) if f.endswith(".scn") ]
 for f in filelist:
     os.remove(os.path.join(loc_TS4, f))
-
 filelist = [ f for f in os.listdir(loc_TS5) if f.endswith(".scn") ]
 for f in filelist:
     os.remove(os.path.join(loc_TS5, f))
 
-# Create batch files
-batch_TS0 = open(loc_TS0 + "/batch.scn", "w")
-batch_TS1 = open(loc_TS1 + "/batch.scn", "w")
-batch_TS2 = open(loc_TS2 + "/batch.scn", "w")
-batch_TS3 = open(loc_TS3 + "/batch.scn", "w")
-batch_TS4 = open(loc_TS4 + "/batch.scn", "w")
-batch_TS5 = open(loc_TS5 + "/batch.scn", "w")
-
 # Loop over mission
 for i in range(parameters.N_missions):
-    # Create scenario files
-    scn_TS0 = open(loc_TS0 + "/test" + str(i) + ".scn", "w")
-    scn_TS1 = open(loc_TS1 + "/test" + str(i) + ".scn", "w")
-    scn_TS2 = open(loc_TS2 + "/test" + str(i) + ".scn", "w")
-    scn_TS3 = open(loc_TS3 + "/test" + str(i) + ".scn", "w")
-    scn_TS4 = open(loc_TS4 + "/test" + str(i) + ".scn", "w")
-    scn_TS5 = open(loc_TS5 + "/test" + str(i) + ".scn", "w")
-
     # Write scenario files
-    # create first UAV in scenario for wind calm series
-    start_lat0_nowind, start_lon0_nowind = geo.qdrpos(lat_ref, lon_ref, scenario_data[i]['start_qdr0_nowind'], scenario_data[i]['start_dist0_nowind']  / nm)
+    # Create first UAV in scenario for wind calm test series
+    start_lat0_nowind, start_lon0_nowind = geo.qdrpos(lat_ref, lon_ref, scenario_data[i]['start_qdr0_nowind'], scenario_data[i]['start_dist0_nowind'] / nm)
     start_lat1_nowind, start_lon1_nowind = geo.qdrpos(lat_ref, lon_ref, scenario_data[i]['start_qdr1_nowind'], scenario_data[i]['start_dist1_nowind'] / nm)
     start_lat0_wind, start_lon0_wind = geo.qdrpos(lat_ref, lon_ref, scenario_data[i]['start_qdr0_wind'], scenario_data[i]['start_dist0_wind'] / nm)
     start_lat1_wind, start_lon1_wind = geo.qdrpos(lat_ref, lon_ref, scenario_data[i]['start_qdr1_wind'], scenario_data[i]['start_dist1_wind'] / nm)
@@ -179,88 +162,32 @@ for i in range(parameters.N_missions):
     # Create wind
     wind_line = "00:00:00.00>WIND 0 0 1000 " + str(wind_data[i]['direction']) + " " + str(wind_data[i]['speed'] / kts) + "\n"
 
-    # Write scenario files
-    scn_TS0.write(cre_line_wind_calm + conf_line_wind_calm + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
-    scn_TS1.write(cre_line_wind_calm + conf_line_wind_calm + bank_limit_lines + flyturn_lines + wpt_lines)
-    scn_TS2.write(cre_line_wind_calm + conf_line_wind_calm + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
-    scn_TS3.write(wind_line + cre_line_wind + conf_line_wind + bank_limit_lines + flyturn_lines + wpt_lines)
-    scn_TS4.write(wind_line + cre_line_wind + conf_line_wind + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
-    scn_TS5.write(wind_line + cre_line_wind + conf_line_wind + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
-
-    # write batch files of check testseries (1 and 5)
-    batch_TS0.write("00:00:00.00>SCEN test_" + "{:06d}".format(i) + "_TS0\n")
-    batch_TS0.write("00:00:00.00>PCALL Thesis/TS0/test" + str(i) + ".scn\n")
-    batch_TS0.write("00:00:00.00>INIT_LOGGERS 0 0\n")
-    batch_TS0.write("00:00:00.00>FF\n")
-    batch_TS0.write("00:00:00.00>SCHEDULE 00:03:00.00 STOP_LOGGERS\n")
-    batch_TS0.write("00:00:00.00>SCHEDULE 00:03:00.00 HOLD\n")
-
-    batch_TS5.write("00:00:00.00>SCEN test_" + "{:06d}".format(i) + "_TS5\n")
-    batch_TS5.write("00:00:00.00>PCALL Thesis/TS5/test" + str(i) + ".scn\n")
-    batch_TS5.write("00:00:00.00>INIT_LOGGERS 5 0\n")
-    batch_TS5.write("00:00:00.00>FF\n")
-    batch_TS5.write("00:00:00.00>SCHEDULE 00:03:00.00 STOP_LOGGERS\n")
-    batch_TS5.write("00:00:00.00>SCHEDULE 00:03:00.00 HOLD\n")
-
     for j in range(parameters.N_RS):
-        batch_TS1.write("00:00:00.00>SCEN test_" + "{:06d}".format(i) + "_TS1_RS" + str(j + 1) + "\n")
-        batch_TS2.write("00:00:00.00>SCEN test_" + "{:06d}".format(i) + "_TS2_RS" + str(j + 1) + "\n")
-        batch_TS3.write("00:00:00.00>SCEN test_" + "{:06d}".format(i) + "_TS3_RS" + str(j + 1) + "\n")
-        batch_TS4.write("00:00:00.00>SCEN test_" + "{:06d}".format(i) + "_TS4_RS" + str(j + 1) + "\n")
+        # Create scenario files
+        scn_TS0 = open(loc_TS0 + "/test" + "{:06d}".format(i) + "_RS" + str(j+1) + ".scn", "w")
+        scn_TS1 = open(loc_TS1 + "/test" + "{:06d}".format(i) + "_RS" + str(j+1) + ".scn", "w")
+        scn_TS2 = open(loc_TS2 + "/test" + "{:06d}".format(i) + "_RS" + str(j+1) + ".scn", "w")
+        scn_TS3 = open(loc_TS3 + "/test" + "{:06d}".format(i) + "_RS" + str(j+1) + ".scn", "w")
+        scn_TS4 = open(loc_TS4 + "/test" + "{:06d}".format(i) + "_RS" + str(j+1) + ".scn", "w")
+        scn_TS5 = open(loc_TS5 + "/test" + "{:06d}".format(i) + "_RS" + str(j+1) + ".scn", "w")
 
         common_lines = ""
         common_lines += "00:00:00.00>ASAS ON\n"
         common_lines += "00:00:00.00>RESO SSDUAV\n"
         common_lines += "00:00:00.00>PRIORULES ON RS" + str(j + 1) + "\n"
 
-        batch_TS1.write(common_lines)
-        batch_TS2.write(common_lines)
-        batch_TS3.write(common_lines)
-        batch_TS4.write(common_lines)
+        # Write scenario files
+        scn_TS0.write(cre_line_wind_calm + conf_line_wind_calm + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
+        scn_TS1.write(common_lines + cre_line_wind_calm + conf_line_wind_calm + bank_limit_lines + flyturn_lines + wpt_lines)
+        scn_TS2.write(common_lines + cre_line_wind_calm + conf_line_wind_calm + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
+        scn_TS3.write(common_lines + wind_line + cre_line_wind + conf_line_wind + bank_limit_lines + flyturn_lines + wpt_lines)
+        scn_TS4.write(common_lines + wind_line + cre_line_wind + conf_line_wind + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
+        scn_TS5.write(wind_line + cre_line_wind + conf_line_wind + bank_limit_lines + flyturn_lines + wpt_lines + gf_lines)
 
-        batch_TS1.write("00:00:00.00>PCALL Thesis/TS1/test" + str(i) + ".scn\n")
-        batch_TS2.write("00:00:00.00>PCALL Thesis/TS2/test" + str(i) + ".scn\n")
-        batch_TS3.write("00:00:00.00>PCALL Thesis/TS3/test" + str(i) + ".scn\n")
-        batch_TS4.write("00:00:00.00>PCALL Thesis/TS4/test" + str(i) + ".scn\n")
-
-        # Use logging plugin to log
-        batch_TS1.write("00:00:00.00>INIT_LOGGERS 1 " + str(j + 1) + "\n")
-        batch_TS2.write("00:00:00.00>INIT_LOGGERS 2 " + str(j + 1) + "\n")
-        batch_TS3.write("00:00:00.00>INIT_LOGGERS 3 " + str(j + 1) + "\n")
-        batch_TS4.write("00:00:00.00>INIT_LOGGERS 4 " + str(j + 1) + "\n")
-
-        # Fast forward
-        batch_TS1.write("00:00:00.00>FF\n")
-        batch_TS2.write("00:00:00.00>FF\n")
-        batch_TS3.write("00:00:00.00>FF\n")
-        batch_TS4.write("00:00:00.00>FF\n")
-
-        # end the loggers and save file
-        batch_TS1.write("00:00:00.00>SCHEDULE 00:03:00.00 STOP_LOGGERS\n")
-        batch_TS2.write("00:00:00.00>SCHEDULE 00:03:00.00 STOP_LOGGERS\n")
-        batch_TS3.write("00:00:00.00>SCHEDULE 00:03:00.00 STOP_LOGGERS\n")
-        batch_TS4.write("00:00:00.00>SCHEDULE 00:03:00.00 STOP_LOGGERS\n")
-
-        batch_TS1.write("00:00:00.00>SCHEDULE 00:03:00.00 HOLD\n")
-        batch_TS2.write("00:00:00.00>SCHEDULE 00:03:00.00 HOLD\n")
-        batch_TS3.write("00:00:00.00>SCHEDULE 00:03:00.00 HOLD\n")
-        batch_TS4.write("00:00:00.00>SCHEDULE 00:03:00.00 HOLD\n")
-
-    # Close scenario files
-    scn_TS0.close()
-    scn_TS1.close()
-    scn_TS2.close()
-    scn_TS3.close()
-    scn_TS4.close()
-    scn_TS5.close()
-
-# Close batch files
-batch_TS0.close()
-batch_TS1.close()
-batch_TS2.close()
-batch_TS3.close()
-batch_TS4.close()
-batch_TS5.close()
-
-
-
+        # Close scenario files
+        scn_TS0.close()
+        scn_TS1.close()
+        scn_TS2.close()
+        scn_TS3.close()
+        scn_TS4.close()
+        scn_TS5.close()
